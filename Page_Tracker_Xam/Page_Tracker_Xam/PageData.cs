932899ab52514c5f90036c205bde68cc93d4dd3a
garv13 <garvjasuja@outlook.com>
2018-06-03 23:00:39 +0530

    Base Class added

---
+++ Page_Tracker_Xam/Page_Tracker_Xam/PageData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Page_Tracker_Xam
+{
+    class PageData
+    {
+        public string Checksum { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int RefreshMinutes { get; set; }
+
+        public DateTime LastRefreshTime { get; set; }
+
+        public bool IsRefresh {
+            get {
+                if (LastRefreshTime.AddMinutes(RefreshMinutes) >= DateTime.Now)
+                    return true;
+                else
+                    return false;
+            }
+            set {
+                throw new Exception("Not Settable");
+            }
+        }
+    }
+}
